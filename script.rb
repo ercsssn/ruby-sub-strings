3fd8815ae954b664fa4621cbb01063a8cf46bc42
John Ericsson <ercsssn@gmail.com>
2022-07-11 17:38:50 +0800

    Add the if else logic to filter non-exact words

--- script.rb
+++ script.rb
@@ -5,27 +5,41 @@ def substrings(string, words_array)
 
     string_array = string.gsub(/[^0-9A-Za-z\s]/, '').downcase.split(" ")
 
+    match_array  = []
+
     string_array.each do |word|
 
         letters_array = word.split("")
         puts "WORD #{letters_array}"
+        word_test_array = []
 
         words_array.each do |substring|
             sub_letters_array = substring.split("")
 
             if sub_letters_array.intersect?(letters_array) #Check if there is at least one character in common
-                
+
                if sub_letters_array == letters_array
-                puts substring 
-               end
-                # puts "TO MATCH #{sub_letters_array}"
+                match_array.append(substring)  #Get exact match
+               else
+                puts substring
                 array_match = sub_letters_array & letters_array
-                # p array_match
-            else
-                puts "No letters matched"
+                word_test = array_match.join("")
+
+                if words_array.include?(word_test)
+                    
+                    if (!word_test_array.include?(word_test))
+                        word_test_array.append(word_test)
+                        match_array.append(word_test)
+                    end
+                end
+               end
+            # else
+            #     puts "No letters matched"
             end
         end
 
       end
+
+      p match_array
 
 end
